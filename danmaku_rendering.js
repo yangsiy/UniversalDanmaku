b0ba2184dbff2e27455679a8de0bb32f42874114
chenYuheng <yuheng@tataufo.com>
2015-05-31 17:32:37 +0800

    改进了弹幕的显示机制

--- danmaku_rendering.js
+++ danmaku_rendering.js
@@ -4,6 +4,11 @@ var win_Hei= $(window).height();//窗口宽
 console.log('danmaku rendering init'+win_Wid);
 DM_init();
 
+//下一条弹幕显示的Y轴坐标
+var next_top = 10;
+//当前板上的弹幕总数
+var num_on_board = 0;
+
 //调整浏览器后重排元素
 $(window).resize(function () {
     win_Hei = $(window).height();
@@ -56,20 +61,22 @@ function DM_init() {
         "z-index":"0"
     });
     
-
-    test();
-
+    for (var i =0;i<50;i++)
+    {
+        setTimeout(function(){
+            test();
+        },300*i);
+    }
 };
 
 function test(){
 
-    console.log('test init');
     var myDate = new Date();//时间种子
     var mytime=myDate.getTime(); 
     var data2 = {
         id:mytime,
-        color:'red',
-        text:'测试弹幕'+mytime
+        color:randomColor,
+        text:'亮瞎眼亮瞎眼亮瞎眼'+mytime
     };
 
     var dm2 = createDM(data2);
@@ -84,33 +91,70 @@ function test(){
 function createDM(DM_data){
     console.log(danmaku_is_on);
     if(danmaku_is_on != true) return false;
+    if (next_top>=220)
+    {
+        next_top = 10;
+    }
     var dm = new Object();
     dm.id = DM_data.id;
     dm.color = DM_data.color;
     dm.text = DM_data.text;
+    var left_length = 0 - dm.text.length * 20;
     //显示弹幕
     dm.show = function(){
-        console.log('ferferi');
-    var select = "#"+dm.id;
-    $("#DM").append("<div id='"+dm.id+"'></div>");
-    $(select).html(dm.text)
-        .css({
-        "position":"relative",
-        "float":"auto",
-        "left":win_Wid+"px",
-        "font-size":25+"px",
-        "color":dm.color,
-        "margin-top":"10px",
-        "margin-left":"30px",
-        "width":"60px",
-        "overflow":"visible",
-        "white-space":"nowrap",
-        "z-index":"99999"
-        })
-    .stop(true,false).animate({'left':-1000+'px'},10000,"linear",
+        var select = "#"+dm.id;
+        $("#DM").append("<div id='"+dm.id+"'></div>");
+        num_on_board += 1;
+        $(select).html(dm.text)
+            .css({
+            "position":"absolute",
+            "float":"auto",
+            "left":win_Wid+"px",
+            "font-size":25+"px",
+            "color":dm.color,
+            "margin-top":next_top + "px",
+            "margin-left":"0px",
+            "width":"60px",
+            "overflow":"visible",
+            "white-space":"nowrap",
+            "z-index":"99999"
+        }) 
+    .stop(true,false).animate({'left': left_length +'px'},8000,"linear",
         setTimeout(function(){
             $(select).remove();
-        },15000))
+            num_on_board -= 1;
+            if(num_on_board<=7)
+            {
+                next_top = 10;
+            }
+        },8000))
     };
+    next_top += 30;
     return dm;
 };
+
+function randomColor()
+{
+    var i = rd();
+    switch(i)
+    {
+        case 1:
+        return 'orange';
+        break;
+        case 2:
+        return 'green';
+        break;
+        case 3:
+        return 'red';
+        break;
+        case 4:
+        return 'black';
+        break;
+        case 5:
+        return 'purple';
+        break;
+        default:
+        return 'blue';
+        break;
+    }
+}
